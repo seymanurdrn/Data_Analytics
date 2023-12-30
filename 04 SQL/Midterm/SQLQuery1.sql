5f9702d1b77e515d3ac78f29cde201ecc2a88a02
seymanurdrn <150393396+seymanurdrn@users.noreply.github.com>
2023-12-30 13:59:20 +0300

    Create SQLQuery1.sql

---
+++ 04 SQL/Midterm/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿VERİ EKLEME
+INSERT INTO cars (sütun isimleri)
+VALUES (SELECT NEXTVAL('..id'), ' ')
+
+
+CREATE SEQUENCE cars_id her istenen sütun için toplamda 4 tane lazım 
+SATRT 100
+INCREMENT 1
+MİNVALUE 101
+OWNED BY cars.id;
+
+
+sütunları oluşturduktan sonra parantezi kapamadan altına
+CONSTRAINT make CHECK (make>1 yada make IN ('Toyota' 'Honda') bunlardan biri olmalı
