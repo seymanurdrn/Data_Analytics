4eb409acdd84d06cdb5a1b01a8aa384fbf33077e
seymanurdrn <150393396+seymanurdrn@users.noreply.github.com>
2024-01-05 15:51:18 +0300

    Update fileio.py

--- 03 Python/fileio.py
+++ 03 Python/fileio.py
@@ -1,9 +1,47 @@
+import csv
+import time
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time:0.3f} seconds to execute")
+        return result
+    return wrapper
+
 class FileIO:
-    def __init__(self, filename):
+    def __init__(self,filename):
         self.filename = filename
 
+    @timing_decorator
     def read(self):
-        with open(self.filename, "r") as file:
+        with open(self.filename, 'r') as file: #with file oluştururken ve with'in altındaki işlemler bittikten sonra 2 işlemi otomatik hallediyor. 1) File'ı her açtığımda close etmem kapatmam lazım. With bunu benim yerime yapıyor. Enter ve Exit'da yapılması gereken işlemleri hallediyor.
             return file.read()
+ 
+    @timing_decorator
+    def write(self, data):
+        with open(self.filename, 'w') as file:
+            return file.write(data)
         
+    def append(self,data):
+        with open(self.filename, 'a') as file:
+            return file.write(data)
+    
+
+class FileCSV:
+    def __init__(self,filename):
+        self.filename = filename
+
+    @timing_decorator
+    def read(self):
+        with open(self.filename, 'r') as file:
+            csv_data = csv.reader(file)
+            return list(csv_data)
+    
+    @timing_decorator
+    def read_dict(self):
+        with open(self.filename, 'r') as file:
+            books = csv.DictReader(file)
+            return list(books)
         
